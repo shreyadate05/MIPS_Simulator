a32663babb73f4678f23345c970b8e2e523074ae
shreyadate05 <shreyadate05@gmail.com>
2020-11-30 04:28:21 -0500

    adding input parser

---
+++ MIPSParser.py
@@ -0,0 +1,22 @@
+import sys
+from InstructionHelper import parseInstruction
+from utils import parseFile
+
+def parseInstruction(inst):
+
+
+def parseInstFile(instFile):
+    instList = parseFile(instFile)
+    instructions = []
+    for inst in instList:
+        inst = parseInstruction(inst)
+        instructions.append(inst)
+    return instructions
+
+def parseDataFile(dataFile):
+    data = parseFile(dataFile)
+    return data
+
+def parseConfFile(confFile):
+    configs = parseFile(confFile)
+    return configs
--- simulator.py
+++ simulator.py
@@ -1,34 +1,22 @@
 import sys
-from InstructionHelper import parseInstruction
-from utils import parseFile
+from MIPSParser import parseInstFile
+from MIPSParser import parseDataFile
+from MIPSParser import parseConfFile
 
 ansFile = ""
 data = []
+instructions = []
 
-def parseInstFile(instFile):
-    instructions = parseFile(instFile)
-    for inst in instructions:
-        inst = parseInstruction(inst)
-    print(instructions)
-
-def parseDataFile(dataFile):
-    global data
-    data = parseFile(dataFile)
-
-def parseConfFile(confFile):
-    configs = parseFile(confFile)
-    print(configs)
-
-def init(argv):
+def initMIPSParser(argv):
     global ansFile
     ansFile = argv[4]
-    parseInstFile(argv[1])
-    parseDataFile(argv[2])
-    parseConfFile(argv[3])
+    instructions = parseInstFile(argv[1])
+    data = parseDataFile(argv[2])
+    configs = parseConfFile(argv[3])
 
 def startSimulator(argv):
     try:
-        init(argv)
+        initMIPSParser(argv)
     except:
         print("Something went wrong. Exiting")
         return -1
